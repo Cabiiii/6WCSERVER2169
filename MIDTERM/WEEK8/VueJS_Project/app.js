0298af1cb17507c56fc2bfacfabda1259f5fdce0
Cabii <150228018+Cabiiii@users.noreply.github.com>
2025-08-22 13:02:26 +0800

    Add files via upload

---
+++ MIDTERM/WEEK8/VueJS_Project/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'The Ledger',
+            author: 'Justin Cabe',
+            age: 20
+        }
+    }
+});
+
+app.mount('#app')
